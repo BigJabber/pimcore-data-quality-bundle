8716b2745971bb0ea0f6351038db32191222d4e2
BigJabber <32928920+BigJabber@users.noreply.github.com>
2024-12-06 14:09:15 +0000

    Update UpdateDataQualityCommand.php

--- src/Command/UpdateDataQualityCommand.php
+++ src/Command/UpdateDataQualityCommand.php
@@ -11,6 +11,7 @@ use Pimcore\Model\DataObject\AbstractObject;
 use Pimcore\Model\DataObject\ClassDefinition;
 use Pimcore\Model\DataObject\DataQualityConfig;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
